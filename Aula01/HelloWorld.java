c58b33dc1f26e8d99194fb039ce6bac827dae691
danielvieira95 <engdanielvieira95@gmail.com>
2026-08-03 23:00:28 -0300

    Aula 01 exemplo java

---
+++ Aula01/HelloWorld.java
@@ -0,0 +1,11 @@
+package Aula01;
+
+// Classe chamada HelloWorld
+public class HelloWorld {
+ 
+    // Cria uma função
+    public static void main(String[] args){
+        System.out.println("Hello world"); // imprime informação no console terminal
+    }
+    
+}
